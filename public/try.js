020ccbada3ea33d789171ab1fd4a74964ff1a814
Jason Garber <jg@jasongarber.com>
2009-04-24 06:45:08 -0400

    Refactored JS so there can be multiple watched fields on a page.

--- public/try.js
+++ public/try.js
@@ -1,19 +1,30 @@
-document.observe("dom:loaded", function() {
-  new Form.Element.Observer(
-    'text_field',
-    0.2,  // 200 milliseconds
-    parse_redcloth
-  );
-  $('submit').hide();
+var TryRedClothObserver = Class.create({
+  initialize: function(observable, resultElement, htmlResultElement) {
+    this.observable = $(observable);
+    this.resultElement = $(resultElement);
+    this.htmlResultElement = $(htmlResultElement);
+    this.initObserver();
+  },
+  initObserver: function() {
+    new Form.Element.Observer(
+      this.observable,
+      0.2,  // 200 milliseconds
+      this.parseRedcloth.bind(this)
+    );
+  },
+  parseRedcloth: function(event) {
+    var rcObserver = this;
+    new Ajax.Request('/', {
+      parameters: { text: rcObserver.observable.value },
+      onSuccess: function(response) {
+        rcObserver.resultElement.update(response.responseText);
+        rcObserver.htmlResultElement.update(response.responseText.escapeHTML());
+      }
+    });
+  }
 });
 
-function parse_redcloth(el, value) {
-  new Ajax.Request('/', {
-    parameters: { text: value },
-    onSuccess: function(response) {
-      $('result').update(response.responseText);
-      $('html-result').update(response.responseText.escapeHTML());
-    }
-  });
-  
-}
+document.observe("dom:loaded", function() {
+  new TryRedClothObserver('text_field', 'result', 'html-result');
+  $('submit').hide();
+});
